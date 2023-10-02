78d22ac504c009df3dbbe0dbedc68d8124b9f0ab
Mouhcine Bannani <mouhcine.bannani@gmail.com>
2023-10-02 14:16:28 +0200

    Create a TodoListComponent in the front and fetching data using axios

--- front-end/src/components/ComponentTodoList.jsx
+++ front-end/src/components/ComponentTodoList.jsx
@@ -8,10 +8,16 @@ const ComponentTodoList = () => {
 
     useEffect(() => {
         axios.get('https://127.0.0.1:8000/api/items')
-            .then((response) => response.json())
-            .then((data) => {
-                setTodos(data);
+            .then((response) => {
+                // console.log(response.data);
+                response.data;
+                // console.log(response.data.todos);
+                setTodos(response.data.todos);
             })
+            // .then((data) => {
+            //     // console.log(data);
+            //     setTodos(todos);
+            // })
             .catch((error) => {
                 console.log('Error fetching JSON data: ', error);
             })
@@ -19,7 +25,14 @@ const ComponentTodoList = () => {
 
     return (
         <div>
-            <pre>{JSON.stringify(todos, null, 2)}</pre>
+            {/*<pre>{JSON.stringify(todos, null, 2)}</pre>*/}
+            <ul>
+                {todos.map((todo) => (
+                    <li key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
