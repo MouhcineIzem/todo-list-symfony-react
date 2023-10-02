e8bd489dd7cc1335fbcb189b7af6cb4c9ca9aa5d
Mouhcine Bannani <mouhcine.bannani@gmail.com>
2023-10-02 13:53:19 +0200

    Add Todo Controller that returns a JSonResponse

--- back-end/src/Controller/TodoController.php
+++ back-end/src/Controller/TodoController.php
@@ -3,17 +3,34 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
+//    #[Route('/api/items', name: 'api_items')]
+//    public function getData() {
+//        $data = [
+//            'message' => 'Hello, world',
+//            'timestamp' => time()
+//        ];
+//        return new JsonResponse($data);
+//    }
+
     #[Route('/api/items', name: 'api_items')]
-    public function getData() {
-        $data = [
-            'message' => 'Hello, world',
-            'timestamp' => time()
-        ];
+    public function getJsonData() {
+        $jsonFilePath = $this->getParameter('kernel.project_dir').'/public/data/db.json';
+
+        if (!file_exists($jsonFilePath)) {
+            throw new FileNotFoundException("The JSON file does not exist");
+        }
+
+
+        $jsonData = file_get_contents($jsonFilePath);
+
+        $data = json_decode($jsonData, true);
+
         return new JsonResponse($data);
     }
 }
